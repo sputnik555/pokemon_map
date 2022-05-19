9c5d66ad03ecd6d86c0fa4cf326d085caf3578a8
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-05-19 14:04:24 +0300

    add fields to pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,13 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
     image = models.ImageField(blank=True, null=True)
+    next_evolution = models.ForeignKey('self', on_delete=models.SET_NULL,
+                                       related_name='previous_evolution',
+                                       null=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.title)
